1186427e4d0f8fed48529d81168619af6997bd4e
Summer-the-coder <galaxycraftgames90@gmail.com>
2025-02-16 18:51:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const fight = document.querySelector('.fight');
+const act = document.querySelector('.act');
+const item = document.querySelector('.item');
+const mercy = document.querySelector('.mercy');
+
+let selected = fight;
+const actions = [fight, act, item, mercy];
+
+const selectionSound = new Audio('songs_and_sfx/select-sound.mp3');
+
+// start and loop the song
+const song = new Audio('songs_and_sfx/mettalocrusher.mp3');
+window.addEventListener('click', function playSong() {
+    song.play();
+    window.removeEventListener('click', playSong);
+});
+const loop = setInterval(function() {
+    if (song.ended) {
+        song.play();
+    }
+}, 1000);
+
+window.addEventListener('keydown', function(event) {
+    const pressed = event.key;
+
+    if (['ArrowRight', 'ArrowLeft'].includes(pressed)) {
+        // Reset all buttons to their default state
+        actions.forEach(button => {
+            const buttonName = button.classList.value;
+            button.src = `sprites/${buttonName.toUpperCase()}.png`;
+        });
+
+        // Update the selected button
+        let currIndex = actions.indexOf(selected);
+        if (pressed === 'ArrowRight') {
+            currIndex = (currIndex + 1) % actions.length;
+        } else if (pressed === 'ArrowLeft') {
+            currIndex = (currIndex - 1 + actions.length) % actions.length;
+        }
+        selected = actions[currIndex];
+
+        const buttonName = selected.classList.value;
+        selected.src = `sprites/${buttonName.toUpperCase()}_selected.png`;
+
+        selectionSound.currentTime = 0;
+        selectionSound.play();
+    }
+});
